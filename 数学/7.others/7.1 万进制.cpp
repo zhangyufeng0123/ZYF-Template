1830727bef25c631a1cedc34e20010c228cc5d33
zhangyufeng0123 <1691903105@qq.com>
2019-11-04 14:49:38 +0800

    update

---
+++ 数学/7.others/7.1 万进制.cpp
@@ -0,0 +1,36 @@
+#include <iomanip>
+void factorial(int n)
+{
+    int a[10001];
+    int places, carry, i, j;
+
+    a[0] = 1;
+    places = 0; //当前数的总位数
+    for (i = 1; i <= n; i++)
+    {
+        carry = 0;
+        for (j = 0; j <= places; j++)
+        {
+            a[j] = a[j] * i + carry; //如果是多次幂函数，将i改成数字即可
+            carry = a[j] / 10000;
+            a[j] %= 10000;
+        }
+        if (carry > 0)
+        {
+            places++;
+            a[places] = carry;
+        }
+    }
+    /*
+     * 输出
+     * 最高位原样输出
+     * 其他位小于1000的，高位补0
+     * 需要头文件<iomanip>
+     */
+    cout << a[places];
+    for (i = places - 1; i >= 0; i--)
+    {
+        cout << setw(4) << setfill('0') << a[i];
+    }
+    cout << endl;
+}
